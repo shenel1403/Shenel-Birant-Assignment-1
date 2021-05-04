37e4a8ad35dce0c48350e2002f572068561b20f2
shenel1403 <shenel.birant@ada.ac.uk>
2021-05-04 18:39:14 +0100

    section a number 1

--- section-a/section-a.test.js
+++ section-a/section-a.test.js
@@ -1,32 +1,34 @@
 // Not all functions are imported, be careful 👇
 const {
-    subtract,
-    divide, 
-    isDivisibleBy,
-    gradeAssignment,
-    areaOrPerimeter
+	subtract,
+	divide,
+	isDivisibleBy,
+	gradeAssignment,
+	areaOrPerimeter,
 } = require("./section-a.js");
 
 // Section A 01. Wrtite a smoke test
-
+// refer to smoke.test.js file
 // Section A 02. Test the subtraction() function
 // Feel free to delete all of the starter test block and make it better
+//Test the subtract() function (provided). Make sure you tested it with different
+// edge cases. Have a think - what do we expect when we minus a negative number?
+// Here is your chance to use multiple assertions within one test.
 
-describe('Test the ', () => {
-    test("Subtract 2 positive numbers, where the first parameter is larger than the scond one", () => {
-        let num1 = 5;
-        let num2 = 1;
-        var expected = 4;
-        var output = subtract(num1, num2);
-    });
+describe("Test the subtract function", () => {
+	test("Subtract 2 positive numbers, where the first parameter is larger than the scond one", () => {
+		let num1 = 5;
+		let num2 = 1;
+		var expected = 4;
+		var output = subtract(num1, num2);
+		expected(subtract(num1 - num2).toBe(expected));
+	});
 });
 
 // Section A 03. Test the divide() function
 // Feel free to delete all of the starter test block and make it better
 
-it('Should divide numbers and not allow division by 0', () => {
-
-});
+it("Should divide numbers and not allow division by 0", () => {});
 
 // Section A 04. Test the isDivisibleBy() function
 
---
+++ section-a/smoke.test.js
@@ -0,0 +1,4 @@
+test("Check that tests are working", () => {
+	expect(5 + 5).toBe(10);
+});
+// only included one assertionas this is what was recommended during our lectures
